55b08d2f4b6d5622efc798efcc7bc82e3234eb23
Leonel Galan <leonel@smashingboxes.com>
2011-09-24 11:21:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OlympicGames::Application.config.secret_token = '78c46648c188c1763523db83976efcc8d4ae12fa4ed1e46c334e494e5c25cbe2d8883cd895fc73d6352d03c6fe5fee42058d09a361ab3aef659b49d81de5783b'
