52e929d33717bf6004688858f0bcf482ab32fde2
Leonel Galan <leonel@smashingboxes.com>
2011-09-24 11:41:33 -0400

    Create countries table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110924153050) do
+ActiveRecord::Schema.define(:version => 20110924153704) do
+
+  create_table "countries", :force => true do |t|
+    t.string   "name"
+    t.string   "code"
+    t.integer  "game_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "games", :force => true do |t|
     t.string   "country"
