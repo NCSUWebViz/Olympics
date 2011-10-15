a63a0e301a5d70709331af7774f6b190c4b15fac
Leonel Galan <leonel@smashingboxes.com>
2011-10-15 12:33:32 -0400

    Fixes belongs_to relation name for Country and Medal

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,6 +2,6 @@ class Country < ActiveRecord::Base
   attr_accessible :name, :code, :gold, :silver, :bronze, :game_id
   # ---------------------------------------------------------------------------
   # Associations
-  belongs_to :games
+  belongs_to :game
   has_many :medals
 end
--- app/models/medal.rb
+++ app/models/medal.rb
@@ -2,5 +2,5 @@ class Medal < ActiveRecord::Base
   attr_accessible :sport, :event, :athlete, :result, :medal, :country_id
   # ---------------------------------------------------------------------------
   # Associations
-  belongs_to :countries
+  belongs_to :country
 end
