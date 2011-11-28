abdcdaf355438586e2c992bcb4b2ba7fa143dbcd
Leonel Galan <leonel@smashingboxes.com>
2011-11-28 16:26:32 -0500

    Added new fields to group by and remove specific fields from select

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -7,7 +7,7 @@ class SportsController < ApplicationController
   end
 
   def show
-    @games = Game.select('games.id, games.country, games.city, games.year, games.season, count(medals.id) AS sport_medal_count').group('games.id, games.country, games.city, games.year, games.season, medals.sport').joins(:countries => :medals).where(:medals => {:sport => params[:id]})
+    @games = Game.select('games.*, count(medals.id) AS sport_medal_count').group('games.id, games.country, games.city, games.year, games.season, games.url, games.created_at, games.updated_at, medals.sport').joins(:countries => :medals).where(:medals => {:sport => params[:id]})
     respond_with(@games)
   end
 end
