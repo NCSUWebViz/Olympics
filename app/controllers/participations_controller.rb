523c51879dac6d2abaea4232dd1b98ff097b8c70
Leonel Galan <leonel@smashingboxes.com>
2011-12-11 20:57:39 -0500

    Events json

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,19 +2,17 @@ class ParticipationsController < ApplicationController
   respond_to :html, :json
 
   def index
-    # if params[:code].present? and params[:year].present?
-    #       @country = Country.joins(:game).where(:code => params[:code], :games => {:year => params[:year]}).try(:first)
-    #       raise ActiveRecord::RecordNotFound if @country.nil?
-    #       redirect_to country_path(@country, :format => 'json')
-    #       return
-    #     end 
-    
     @participations = Participation.all
     respond_with(@participations)
   end
 
   def show
     @participation = Participation.find(params[:id])
+    @sports = @participation.medals.includes(:event => :sport).group_by {|medals| medals.event.sport.name }
+    unless params[:sport_id].nil?
+      @sport = Sport.find(params[:sport_id])
+      @events = @participation.medals.includes(:event).joins(:event).where('events.sport_id = ?', @sport.id).group_by {|medals| medals.event.name }
+    end
     respond_with(@participation)
   end
 end
