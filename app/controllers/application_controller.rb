3fe7f72d59448c82620a274e9311140eb57cddd4
Leonel Galan <leonel@smashingboxes.com>
2011-12-11 20:42:27 -0500

    Cross Domain Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :add_cross_domain_headers
+  
+  def add_cross_domain_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+  end
 end
